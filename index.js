ff1d6e1d8e099c9e7011a5d354fb0463b3ab1375
Kshitiz Khanal <khanal.xitiz@gmail.com>
2023-05-22 21:08:18 +0300

    this is change in code

---
+++ index.js
@@ -0,0 +1,5 @@
+function calculate() {
+    const userInput = document.getElementById("input").value;
+    const length = userInput.trim().length;
+    document.getElementById("result").textContent = Number of characters: ${length};
+  }
